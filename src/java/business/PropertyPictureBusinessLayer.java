16e083ec7f4801ace19625e9d4232b70f75c711e
29751 <dll891234>
2018-11-07 13:11:32 -0500

    Upload property's picture in Host.(only one for now) Read picture in RenterSearchingList

---
+++ src/java/business/PropertyPictureBusinessLayer.java
@@ -0,0 +1,63 @@
+/**
+ * File: PropertyPictureBusinessLayer.java
+ * Description:This is an abstract Data Access Object interface defines the standard operations to be performed.
+ * Create: Oct,28,2018
+ * Author: Liangliang Du
+ * Clients: Michelle Bilek,Farheen Khan
+ * Course: Software Development Project
+ * Professor: Dr. Anu Thomas
+ * Project: A Home to Share
+ * Copyright @ 2018
+ */
+package business;
+
+
+import dataaccess.PropertyPictureDAO;
+import dataaccess.PropertyPictureDAOImpl;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.util.List;
+import transferobjects.PropertyPicture;
+
+/**
+ *
+ * @author 29751
+ */
+public class PropertyPictureBusinessLayer {
+    
+    private PropertyPictureDAO pPictureDAO = null;
+    
+    public PropertyPictureBusinessLayer() {
+        pPictureDAO = new PropertyPictureDAOImpl();
+    }
+      
+    public List<PropertyPicture> getAllPictures(){
+        return pPictureDAO.getAllPictures();
+    }
+
+    public void addPicture(PropertyPicture propertypicture,InputStream blob) throws ValidationException {
+        try {
+            validateFields(propertypicture);
+            pPictureDAO.addPicture(propertypicture,blob);
+        }
+        catch(ValidationException e) {
+            throw e;
+        }
+    }
+    
+    public PropertyPicture getPictureById(int pictureId){
+        return pPictureDAO.getPictureById(pictureId);
+    } 
+    
+    public void deletePicture(int pictureId){
+        pPictureDAO.deletePicture(pictureId);
+    }
+    
+    public Blob getFirstPictureByProperty(int propertyId) {
+         return pPictureDAO.getFirstPictureByProperty(propertyId);
+     }
+    
+    private void validateFields(PropertyPicture propertypicture) throws ValidationException {
+        // check int value whether out of range
+    }  
+}
---
+++ web/display_picture.jsp
@@ -0,0 +1,45 @@
+<%-- 
+File: hostrPofile.jsp
+Description: A page for logged-in hosts to view and edit profile details
+Create: Nov.01,2018
+Author: Liangliang Du
+Clients: Michelle Bilek, Farheen Khan
+Course: Software Development Project
+Professor: Dr. Anu Thomas
+Project: A Home to Share
+Copyright @ 2018
+--%>
+
+<%@page import="business.PropertyPictureBusinessLayer"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ page import="java.io.*"%> 
+<%@ page import="java.sql.*, javax.sql.*" %> 
+<%@ page import="java.util.*"%> 
+<%@ page import="java.math.*"%> 
+<% 
+    String property_no = request.getParameter("property_no"); 
+
+    PropertyPictureBusinessLayer pPictureLayer = new PropertyPictureBusinessLayer();
+    Blob picture = pPictureLayer.getFirstPictureByProperty(Integer.valueOf(property_no));
+    if(picture != null){
+        long size = picture.length(); 
+        //out.print(size); 
+        byte[] bs = picture.getBytes(1, (int)size); 
+        response.setContentType("image/*"); 
+        OutputStream outs = response.getOutputStream(); 
+        outs.write(bs); 
+        outs.flush(); 
+         
+        outs.close();  
+        outs = null;  
+        response.flushBuffer();//下面这三句是一定要加上的，否则就会报错。  
+        out.clear();  
+        out = pageContext.pushBody();
+    
+    }
+
+else { 
+//response.sendRedirect("./images/error.gif"); 
+
+} 
+%> 
